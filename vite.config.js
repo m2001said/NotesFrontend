bf8d20158495c554ef8482d03bf74eecda031b41
Mohamed Said <ms277712@gmail.com>
2022-10-31 21:47:27 +0200

    adding link of notesbackend

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         // target: "http://localhost:8000",
-        target: "https://notesbackend-59jh.onrender.com/",
+        target: "https://notesbackend-59jh.onrender.com",
       },
     },
   },
