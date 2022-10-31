529164046fd07aae9a13e3b5db615dce1bd617d7
Mohamed Said <ms277712@gmail.com>
2022-10-31 21:23:47 +0200

    modify vite config for target backend

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        // target: "http://localhost:8000",
+        target: "https://notesbackend-59jh.onrender.com/",
       },
     },
   },
